d4fb23d642f53fca916de0569d747d90bc64c49c
jrlayton <jreaylayton@gmail.com>
2024-05-07 21:27:09 -0700

    Add onLoad window event listener, displayBooks and createBookMarkup methods

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-import { Book } from "./book";
-import { Library } from "./library";
-
 const myLibrary = new Library();
+
+window.addEventListener("load", displayBooks);
 
 myLibrary.addBook(
   new Book("Dune", "Frank Herbert", 896, false, [
@@ -21,6 +20,29 @@ myLibrary.addBook(
   ])
 );
 
-/*                     “In the case of good books, the point is not to see how many
-                    of them you can get through, but rather how many can get
-                    through to you.” */
+function displayBooks() {
+  const libraryGrid = document.querySelector(".library-grid");
+  myLibrary.getBooks().forEach((book) => {
+    libraryGrid.appendChild(createBookMarkup(book));
+  });
+}
+
+function createBookMarkup(book) {
+  const element = document.createElement("div");
+  element.setAttribute("class", "book");
+  element.setAttribute("data-id", book.getID());
+
+  const title = document.createElement("span");
+  title.innerText = book.getTitle();
+  element.appendChild(title);
+
+  const author = document.createElement("span");
+  author.innerText = book.getAuthor();
+  element.appendChild(author);
+
+  const pages = document.createElement("span");
+  pages.innerText = book.getPages();
+  element.appendChild(pages);
+
+  return element;
+}
