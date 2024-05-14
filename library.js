fcb121f74f0118b9263cc103409803e89a3dea2f
jrlayton <jreaylayton@gmail.com>
2024-05-13 21:27:22 -0700

    Add prev books and method to restore books

--- library.js
+++ library.js
@@ -1,19 +1,31 @@
 class Library {
   constructor() {
     this.books = [];
+    this.prevBooks = [];
   }
 
   addBook(book) {
     this.books.push(book);
+    this.prevBooks.push(book);
   }
 
   removeBook(book) {
     const newBooks = this.books.filter((b) => b.id !== book.id);
+    const newPrevBooks = this.books.filter((b) => b.id !== book.id);
     this.books = newBooks;
+    this.prevBooks = newPrevBooks;
   }
 
   getBooks() {
     return this.books;
+  }
+
+  setBooks(books) {
+    this.books = books;
+  }
+
+  restore() {
+    this.books = this.prevBooks;
   }
 
   removeAllBooks() {
